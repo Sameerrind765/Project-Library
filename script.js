5abba47ef71865595c6400781274ff96fb9234bc
sameerrind765 <sameerrind789@gmail.com>
2024-03-20 02:15:21 +0500

    Now Books Are stored in A object

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-const listContainer = document.getElementById("list-container");
+class MakeBook {
+  constructor(Name, Author, Pages) {
+    this.Name = Name;
+    this.Author = Author;
+    this.Pages = Pages;
+  }
+}
 
-function addBook() {
+function addBook(event) {
   event.preventDefault(); // Prevent default form submission
-
   let bookName = document.getElementById("bookName").value;
   let bookAuthor = document.getElementById("bookAuthor").value;
   let bookPages = document.getElementById("bookPages").value;
-
-  // Validate input (optional)
+  
   if (!bookName || !bookAuthor || !bookPages) {
     alert("Please fill in all book details.");
+    return; // Stop execution if book details are incomplete
   }
-
-  const book = { bookName, bookAuthor, bookPages };
+  const book = new MakeBook(bookName, bookAuthor, bookPages);
+  console.log(book);
   list(book);
 }
 
+const listContainer = document.getElementById("list-container");
 document.getElementById("book-details").addEventListener("submit", addBook);
 
 function list(book) {
   const bookList = document.createElement("div");
   bookList.classList.add("book-item");
   bookList.innerHTML = `
-    <div class="booklist">BOOK NAME; ${book.bookName}
-    <br>AUTHOR NAME; ${book.bookAuthor}
-    <br>NO. OF PAGES; ${book.bookPages} pages</div>
+    <div class="booklist">BOOK NAME: ${book.Name}<br>
+    AUTHOR NAME: ${book.Author}<br>
+    NO. OF PAGES: ${book.Pages} pages</div>
   `;
-
   listContainer.appendChild(bookList);
 }
